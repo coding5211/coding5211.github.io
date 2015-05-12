a1ee9e7d67bade126c36c6b0a31f72cb1af1a9b9
coding5211 <yanwenchao5211@163.com>
2015-05-12 18:16:25 +0800

    Update showPic.js

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,6 +1,6 @@
 function showPic(whichpic)
 {
-	var source = which.getAttribute("href");
+	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
 }
